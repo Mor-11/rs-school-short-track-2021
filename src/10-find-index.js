8dc1e21ba98dca6ddae38617d376270883519c70
Zhuchyk Maryna <marinazh122000@gmail.com>
2021-04-09 00:55:55 +0300

    feat: complete task №10

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,9 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let index = array.length;
+  while (--index >= 0) if (array[index] === value) break;
+  return index;
 }
-
 module.exports = findIndex;
